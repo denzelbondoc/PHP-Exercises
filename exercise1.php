22d1dffdf9055123dce88fc32f59361e20a756e6
tatooyie <dbondoc0126@gmail.com>
2022-11-15 19:30:53 +0900

    final

--- exercise1.php
+++ exercise1.php
@@ -1,9 +1,19 @@
-<?php
-//exercise 1
-echo "Twinkle, Twinkle little star.<br>";
-
-$a = "Twinkle";
-$b = "star";
-
-echo "$a, $a little $b."
-?>
+<html> 
+<head> 
+<title>Twinkle Twinkle</title> 
+</head> 
+<body> 
+<?php 
+echo"Twinkle, Twinkle little star. <br/>"; 
+//Note that html tags can be included along with the text to be echoed. 
+$twinkle="Twinkle"; 
+$star="star"; 
+//This will print out exactly the same as the first echo statement. 
+echo"$twinkle, $twinkle little $star.<br/>"; 
+$twinkle="Thunder"; 
+$star="elephant"; 
+/*This one will be different. Exactly what prints will depend 
+on what valued you assigned to the variables.*/ 
+echo"$twinkle, $twinkle little $star."; 
+?> 
+</body>
